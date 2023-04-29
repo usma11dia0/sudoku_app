11b42a39cca8dc98b39435a472adffa38d48fe87
usma11dia0 <usma11dia0@gmail.com>
2023-04-29 15:11:30 +0900

    add input function

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -17,17 +17,63 @@ function highlightCross(row, col, isHighlighted, target){
   }   
 }
 
-// 盤面に行列ハイライトを付与
-for (let i = 0; i < numRows; i++){
-  for (let j = 0; j < numCols; j++){
-    const inputCell = document.getElementById(`sudoku-input-cell-${i}-${j}`);
-    inputCell.addEventListener("mouseover", () => highlightCross(i, j, true, "input"));
-    inputCell.addEventListener("mouseout", () => highlightCross(i, j, false, "input"));
-    const outputCell = document.getElementById(`sudoku-output-cell-${i}-${j}`);
-    outputCell.addEventListener("mouseover", () => highlightCross(i, j, true, "output"));
-    outputCell.addEventListener("mouseout", () => highlightCross(i, j, false, "output"));
+// 
+function handleInput(event){
+  let cell = event.target;
+  let oldValue = cell.getAttribute("data-old-value");
+  let newValue = cell.textContent;
+
+  // 数字かどうかを判定
+  function isNumber(key) {
+    return key >= '0' && key <= '9';
+  }
+
+  // 半角数字以外が入力された場合、古い値に戻す
+  if (!newValue.split('').every(isNumber)) {
+    cell.textContent = oldValue;
+  } else {
+    cell.setAttribute('data-old-value', newValue);
   }
 }
+
+
+function handleKeyDown(event){
+  let cell =event.target
+  let newValue = event.key;
+
+  // エンターキーが押された場合、現在の値を確定
+  if (newValue === 'Enter') {
+    event.preventDefault();
+    cell.blur(); // セルからフォーカスを外す
+    return;
+  }
+
+  // Backspaceの処理
+  if (newValue === 'Backspace') {
+    event.preventDefault();
+    cell.textContent = '';
+    return;
+  }
+}
+
+// 画面読み込み時に各盤面にイベントリスナーを付与
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i < numRows; i++){
+    for (let j = 0; j < numCols; j++){
+      const inputCell = document.getElementById(`sudoku-input-cell-${i}-${j}`);
+      // 入力盤面
+      inputCell.addEventListener("mouseover", () => highlightCross(i, j, true, "input"));
+      inputCell.addEventListener("mouseout", () => highlightCross(i, j, false, "input"));
+      inputCell.setAttribute("data-old-value",'');
+      inputCell.addEventListener("input", handleInput);
+      inputCell.addEventListener("keydown", handleKeyDown);
+      // 出力盤面
+      const outputCell = document.getElementById(`sudoku-output-cell-${i}-${j}`);
+      outputCell.addEventListener("mouseover", () => highlightCross(i, j, true, "output"));
+      outputCell.addEventListener("mouseout", () => highlightCross(i, j, false, "output"));
+    }
+  }
+})
 
 // 入力盤面
 document.getElementById("input-button").addEventListener("click", async() =>{
