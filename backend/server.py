4d52ad3df339cbe8c6b71a501e81974a42a0290c
usma11dia0 <usma11dia0@gmail.com>
2023-04-29 10:07:43 +0900

    add fontfile

--- backend/server.py
+++ backend/server.py
@@ -8,13 +8,10 @@ class SudokuHandler(BaseHTTPRequestHandler):
     def _send_response(self, content):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(content.encode("utf-8"))
     
-    def end_headers(self):
-        self.send_header("Access-Control-Allow-Origin", "*")
-        super().end_headers()
-
     def do_GET(self):
         if self.path == "/":
             solve_sudoku(input_grid)
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,11 @@
-document.getElementById("sudoku-button").addEventListener("click", async() =>{
+document.getElementById("output-button").addEventListener("click", async() =>{
   const response = await fetch("http://localhost:8000");
-  console.log(response);
   const answer = await response.json();
   console.log(answer)
-  // const answer = await response.text();
-  // document.getElementById("test").value = answer;
-  // document.getElementById("test").innerText = answer;
-  // // 解答を盤面に表示
-  // for (let i = 0; i < 9; i++){
-  //   for (let j = 0; j < 9; j++){
-  //     document.getElementById(`sudoku-cell-${i}-${j}`).value = answer[i][j];
-  //   }
-  // }
+  // 解答を盤面に表示
+  for (let i = 0; i < 9; i++){
+    for (let j = 0; j < 9; j++){
+      document.getElementById(`sudoku-output-cell-${i}-${j}`).innerHTML = answer[i][j];
+    }
+  }
 })
