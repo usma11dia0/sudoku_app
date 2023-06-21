929ba2785f585f51c96bb88d0c9bddd4170481f4
usma11dia0 <usma11dia0@gmail.com>
2023-06-21 21:43:51 +0900

    add css-loader style-loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'public'),
     },
+    plugins:[
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+    ],
   };
